ef16eee35f125cfbb25ebf459e19a1b269d95e90
Thaniyaarrshinii <114228086+Thaniyaarrshinii@users.noreply.github.com>
2023-05-26 10:02:52 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,64 @@
-function App ()
-{
-	return(
-	<div>
-		<h1>App Component</h1>
-	</div>
-	);
-}
-export default App;
+//UNCONTROLLED FORMS
+/*
+import React, { Component } from 'react';  
+class App extends React.Component {  
+  constructor(props) {  
+      super(props);  
+      this.updateSubmit = this.updateSubmit.bind(this);  
+      this.input = React.createRef();  
+  }  
+  updateSubmit(event) {  
+      alert('You have entered the UserName and CompanyName successfully.');  
+      event.preventDefault();  
+  }  
+  render() {  
+    return (  
+      <form onSubmit={this.updateSubmit}>  
+        <h1>Uncontrolled Form Example</h1>  
+        <label>Name:  
+            <input type="text" ref={this.input} />  
+        </label>  
+        <label>  
+            CompanyName:  
+            <input type="text" ref={this.input} />  
+        </label>  
+        <input type="submit" value="Submit" />  
+      </form>  
+    );  
+  }  
+}  
+export default App;  */
+
+
+
+//CONTROLLED FORMS
+
+import React, { Component } from 'react';  
+class App extends React.Component {  
+  constructor(props) {  
+      super(props);  
+      this.state = {value: ''};  
+      this.handleChange = this.handleChange.bind(this);  
+      this.handleSubmit = this.handleSubmit.bind(this);  
+  }  
+  handleChange(event) {  
+      this.setState({value: event.target.value});  
+  }  
+  handleSubmit(event) {  
+      alert('You have submitted the input successfully: ' + this.state.value);  
+      event.preventDefault();  
+  }  
+  render() {  
+      return (  
+          <form onSubmit={this.handleSubmit}>  
+            <h1>Controlled Form Example</h1>  
+            <label>  
+                Name:  
+                <input type="text" value={this.state.value} onChange={this.handleChange} />  
+            </label>  
+            <input type="submit" value="Submit" />  
+         </form>  
+      );  
+  }  
+}  
+export default App;  
